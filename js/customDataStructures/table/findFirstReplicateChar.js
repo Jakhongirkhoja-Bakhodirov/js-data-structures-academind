80b361da34353b359c621ef60da25c9c589c5c5e
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-12-16 18:17:57 +0500

    find first replicate character on string

---
+++ js/customDataStructures/table/findFirstReplicateChar.js
@@ -0,0 +1,15 @@
+const word = "academind";
+
+function findFirstRep(str) {
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        return str[i];
+      }
+    }
+  }
+}
+
+//Time Complexity is O(n^2)
+
+console.log(findFirstRep(word));
