0aee48802ef0e3c62e37318082d0401e2a9408db
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-12-22 08:08:47 +0500

    reproducing collisions hash table

--- js/customDataStructures/table/hash.js
+++ js/customDataStructures/table/hash.js
@@ -21,20 +21,44 @@ class HashTable {
     const keyHash = this.hash(key);
     return this.buckets[keyHash];
   }
-}
 
-const word = "academind";
-
-function findFirstReplicateCharacter(str) {
-  const table = new HashTable();
-  for (const char of str) {
-    if (table.get(char)) {
-      return char;
+  showInfo() {
+    for (const key in this.buckets) {
+      if (this.buckets[key] !== null) {
+        console.log(key, this.buckets[key]);
+      }
     }
-    table.set(char, 1);
   }
 }
 
-//Time Complexity is O(n)
+const table = new HashTable();
 
-console.log(findFirstReplicateCharacter(word));
+for (const char of "academind") {
+  table.set(char, char);
+}
+
+for (const char of "hello") {
+  table.set(char, char);
+}
+
+for (const char of "c++ data structures") {
+  table.set(char, char);
+}
+
+table.showInfo();
+
+// const word = "academind";
+
+// function findFirstReplicateCharacter(str) {
+//   const table = new HashTable();
+//   for (const char of str) {
+//     if (table.get(char)) {
+//       return char;
+//     }
+//     table.set(char, 1);
+//   }
+// }
+
+// //Time Complexity is O(n)
+
+// console.log(findFirstReplicateCharacter(word));
