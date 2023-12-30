0fa94c665d94463820088b600d3f28fe075da95d
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-12-30 08:46:59 +0500

    create add method on binary search tree

--- js/customDataStructures/binary-search-tree/app.js
+++ js/customDataStructures/binary-search-tree/app.js
@@ -1,6 +1,26 @@
 class Node {
   constructor(value) {
     this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+
+  add(value) {
+    if (this.value === null) {
+      this.value = value;
+      return;
+    }
+    if (this.value < value) {
+      const newNode = new Node(value);
+      this.right = newNode;
+      return;
+    }
+
+    if (this.value > value) {
+      const newNode = new Node(value);
+      this.left = newNode;
+      return;
+    }
   }
 }
 
@@ -8,4 +28,12 @@ class Tree {
   constructor() {
     this.root = new Node(null);
   }
+
+  add(value) {
+    this.root.add(value);
+  }
+
+  remove(value) {}
+
+  find(value) {}
 }
