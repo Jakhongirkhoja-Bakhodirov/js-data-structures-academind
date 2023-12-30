17f39cb1cdb3ec0194f5ffe48d4a330581d799f5
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-12-30 11:33:21 +0500

    create find method on binary search tree

--- js/customDataStructures/binary-search-tree/app.js
+++ js/customDataStructures/binary-search-tree/app.js
@@ -22,6 +22,18 @@ class Node {
       return;
     }
   }
+
+  find(value) {
+    if (this.value === value) {
+      return this;
+    }
+    if (this.value < value && this.right) {
+      this.right.find(value);
+    }
+    if (this.value > value && this.left) {
+      this.left.find(value);
+    }
+  }
 }
 
 class Tree {
@@ -33,7 +45,24 @@ class Tree {
     this.root.add(value);
   }
 
-  remove(value) {}
+  find(value) {
+    return this.root.find(value);
+  }
 
-  find(value) {}
+  remove(value) {}
 }
+
+const tree = new Tree();
+tree.add(10);
+tree.add(5);
+tree.add(2);
+tree.add(6);
+tree.add(20);
+tree.add(25);
+tree.add(39);
+
+console.log(tree);
+
+console.log(tree.find(6));
+console.log(tree.find(7));
+console.log(tree.find(39));
